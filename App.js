a23315b57c7eb6f16dcbe03082a525677aad78d3
MacBook <rafa@Ahmads-MacBook-Air.local>
2024-07-04 12:20:38 +0800

    navigasi

--- App.js
+++ App.js
@@ -1,113 +1,36 @@
-import { StyleSheet, View, Text, TextInput, Image } from 'react-native';
-import React from 'react';
-import { useFonts } from 'expo-font';
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Loginpage from './loginpage';
+import signuppage from './signuppage';
+import ForgetPasswordpage from './ForgetPasswordpage';
 
-const ButtonCustom = ({ text, color }) => {
+function HomeScreen({navigation}) {
   return (
-    <View style={{
-      backgroundColor: color,
-      width: '100%',
-      height: 50,
-      borderRadius: 25,
-      justifyContent: 'center',
-      marginTop: 20,
-    }}>
-      <Text style={{
-        textAlign: 'center',
-        color: 'white',
-        fontSize: 18,
-        fontWeight: 'bold',
-      }}>
-        {text}
-      </Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+     <Button title="ke halaman login" onPress={() => navigation.navigate('Login')} /> 
+     <Button title="ke singup" onPress={() => navigation.navigate('singup')} /> 
+     <Button title="ke forget password" onPress={() => navigation.navigate('ForgetPassword')} /> 
     </View>
-  )
+   
+  );
 }
 
-const TextInputCustom = ({ placeholder, typekeyboard }) => {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <TextInput
-      placeholder={placeholder}
-      keyboardType={typekeyboard}
-      style={{
-        width: '100%',
-        height: 50,
-        borderColor: 'gray',
-        borderWidth: 1,
-        borderRadius: 10,
-        marginBottom: 20,
-        paddingLeft: 10,
-        fontSize: 18,
-        fontFamily: 'MetroMedium'
-      }}
-    />
-  )
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={Loginpage} />
+        <Stack.Screen name="singup" component={signuppage} />
+        <Stack.Screen name="ForgetPassword" component={ForgetPasswordpage} /> 
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-const App = () => {
-    const [dapatFont]= useFonts( {
-        'MetroBold' : require('./assets/fonts/Metropolis-Bold.otf'),
-        'MetroMedium' : require('./assets/fonts/Metropolis-Medium.otf')   
-    } )
-    
-  return (
-    <View style={styles.container}>
-      <View style={styles.content}>
-        <Text style={styles.title}>Sign up</Text>
-        <View style={styles.form}>
-          <TextInputCustom placeholder="Name" typekeyboard="default" />
-          <TextInputCustom placeholder="Email" typekeyboard="email-address" />
-          <TextInputCustom placeholder="Password" typekeyboard="default" />
-          <ButtonCustom text="SIGN UP" color="red" />
-        </View>
-      </View>
-      <View style={styles.logoRow}>
-        <View style={styles.logoContainer}>
-          <Image source={require('./assets/facebook.png')} style={styles.logo} />
-        </View>
-        <View style={styles.logoContainer}>
-          <Image source={require('./assets/google.png')} style={styles.logo} />
-        </View>
-      </View>
-    </View>
-  )
-}
-
-export default App
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F0F8FF',
-    padding: 20,
-  },
-  content: {
-    flex: 1,
-    justifyContent: 'center',
-    //alignItems: 'center',
-  },
-  title: {
-    fontSize: 32,
-    //fontWeight: 'bold',
-    marginBottom: 40,
-    fontFamily: 'MetroBold'
-  },
-  form: {
-    width: '100%',
-    alignItems: 'center',
-  },
-  logoRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    paddingVertical: 20,
-  },
-  logoContainer: {
-    backgroundColor: 'white',
-    borderRadius: 15,
-    padding: 10,
-  },
-  logo: {
-    width: 50,
-    height: 50,
-  },
-});
+export default App;
